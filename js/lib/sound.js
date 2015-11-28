243cb9187654769612712412f32e6c67456ffc03
Clonkonaut <Armag3ddon@web.de>
2015-11-28 01:44:31 +0100

    Added sound preloading feature.
    Conflicts:
    	index.html
    	js/lib/sound.js

--- js/lib/sound.js
+++ js/lib/sound.js
@@ -1,12 +1,36 @@
 var sound = {
-	sampels: [],
+	urls: [],
 	urls: [],
 
-	play: function( file ) {
+	add: function( url ) {
+		this.urls.push( url );
+	},
+
+	load: function( callback ) {
+		var total = 0, loaded = 0;
+
+		function complete() {
+			if( ++loaded >= total ) callback();
+		}
+
+		while( this.urls.length ) {
+			var url = this.urls.shift();
+			if( typeof this[url] == 'undefined' ) {
+				total++;
+				this[url] = new Audio(url);
+				this[url].oncanplaythrough = complete;
+				this[url].onended = function() { self.urls[file].push( this ); };
+			}
+		}
+
+		if( total == 0 ) callback();
+	},
+
+	play: function( url ) {
 		var self = this;
 
-		if( !this.sampels[file] )
-			this.sampels[file] = [];
+		if( typeof this[url] != 'undefined' )
+			return this[url].play();
 
 		if( this.sampels[file].length ) {
 			var sample = this.sampels[file].pop();
