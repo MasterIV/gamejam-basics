c44e2382b6a0bd4ff6bb7b979a7b8aeee3bd6e78
Clonkonaut <Armag3ddon@web.de>
2015-12-03 19:45:36 +0100

    Matched sound.js logic to graphic.js

--- js/lib/sound.js
+++ js/lib/sound.js
@@ -1,51 +1,8 @@
 var sound = {
 	urls: [],
-	urls: [],
 
 	add: function( url ) {
 		this.urls.push( url );
-	},
-
-	load: function( callback ) {
-		var total = 0, loaded = 0;
-
-		function complete() {
-			if( ++loaded >= total ) callback();
-		}
-
-		while( this.urls.length ) {
-			var url = this.urls.shift();
-			if( typeof this[url] == 'undefined' ) {
-				total++;
-				this[url] = new Audio(url);
-				this[url].oncanplaythrough = complete;
-				this[url].onended = function() { self.urls[file].push( this ); };
-			}
-		}
-
-		if( total == 0 ) callback();
-	},
-
-	play: function( url ) {
-		var self = this;
-
-		if( typeof this[url] != 'undefined' )
-			return this[url].play();
-
-		if( this.sampels[file].length ) {
-			var sample = this.sampels[file].pop();
-			sample.play();
-			return sample;
-		} else {
-			var sample = new Audio( file );
-			sample.onended = function() { self.sampels[file].push( this ); };
-			sample.play();
-			return s;
-		}
-	},
-
-	add: function(url) {
-		this.urls.push(url);
 	},
 
 	load: function( callback ) {
@@ -59,13 +16,22 @@ var sound = {
 			var url = this.urls.shift();
 			if( typeof this[url] == 'undefined' ) {
 				total++;
-
-				var sample = new Audio(url);
-				sample.oncanplaythrough = complete;
-				this.sampels[url] = [sample];
+				this[url] = new Audio(url);
+				this[url].oncanplaythrough = complete;
+				this[url].onended = function() { self.urls[file].push( this ); };
 			}
 		}
 
 		if( total == 0 && callback ) callback();
-	}
+	},
+
+	play: function( url ) {
+		var self = this;
+
+		if( typeof this[url] != 'undefined' )
+			return this[url].play();
+
+		this[url] = new Audio( url );
+		return this[url].play();
+	},
 }
