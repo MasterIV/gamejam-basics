9cb753373963030c047ed40dea13a4de402731d2
Clonkonaut <Armag3ddon@web.de>
2015-12-04 18:41:04 +0100

    Added Poly and PolyEntity for endless polygon-related fun!

--- js/lib/util.js
+++ js/lib/util.js
@@ -19,3 +19,32 @@ function rad_to_deg( rad ) {
 function arrayRemove( arr, element ) {
 	arr.splice( arr.indexOf( element ), 1 );
 }
+
+// Given three vectors (V2) makes a right headed ray cast from a to line b/c
+// Returns -1 if ray intersects b/c
+// Returns  0 if a is on line b/c
+// Returns +1 if no intersection
+function intersect( a, b, c ) {
+	if ( a.y == b.y && a.y == c.y ) { // a is on same y than b and c
+		if ( (b.x < a.x && a.x < c.x) || (c.x < a.x && a.x < b.x) )
+			return 0;
+		else
+			return 1;
+	}
+	if (b.y > c.y) { // switch b and c
+		var d = c;
+		c = b;
+		b = d;
+	}
+	if ( a.x == b.x && a.y == b.y ) // a == b
+		return 0;
+	if ( a.y < b.y || a.y > c.y ) // a on different y
+		return 1;
+
+	var delta = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+	if (delta > 0)
+		return -1;
+	else if(delta < 0)
+		return 1;
+	return 0;
+}
