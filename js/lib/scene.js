256d5e632e4db3066b5dcfbd1ceba2c8fb7e121b
Dario D. Müller <dario-domi@hotmail.de>
2016-08-26 12:09:53 +0200

    fixed canvas bg wasnt clearing outside debug mode

--- js/lib/scene.js
+++ js/lib/scene.js
@@ -1,17 +1,16 @@
 define(['basic/entity', 'geo/v2', 'core/graphic', 'config/screen', 'config/config'],
 		function (Entity, V2, graphics, screen, config) {
-			function Scene() {
+		    function Scene() {
 				Entity.call(this, Zero(), new V2(screen.w, screen.h));
 				this.keyAware = [];
+			    this.bgColor = "#DDD";
 			}
 
 			Scene.prototype = new Entity();
 
 			Scene.prototype.onDraw = function (ctx) {
-				if (config.debug) {
-					ctx.fillStyle = "#dddddd";
-					ctx.fillRect(0, 0, this.size.x, this.size.y);
-				}
+				ctx.fillStyle = this.bgColor;
+				ctx.fillRect(0, 0, this.size.x, this.size.y);
 				if (this.bg) {
 					ctx.drawImage(graphics[this.bg], 0, 0);
 				}
